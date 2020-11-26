519ea5eab48b71a158a58e732d527867d4c20115
Peng <13750523250@163.com>
2020-11-26 12:12:10 +0800

    增加iPhone12的型号名称映射；增加模拟器型号标识读取；

--- YPCategory/UIKit/UIDevice+YPSysInfo.h
+++ YPCategory/UIKit/UIDevice+YPSysInfo.h
@@ -10,15 +10,25 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
+@interface UIDevice (yp_utsname)
+
+/// 设备硬件号，如"iPhone10,3"。
+- (NSString *)yp_machine;
+
+@end
+
+@interface UIDevice (yp_ModelName)
+
+/// 型号标识。
+- (NSString *)yp_modelIdentifier;
+/// 型号名称。
+- (NSString *)yp_modelName;
+
+@end
+
 @interface UIDevice (yp_Platform)
-
-/// 设备硬件号，如"iPhone10,3"
-- (NSString *)machine;
-
 /// 设备型号
 - (NSString *)deviceType;
-
-- (NSString *)platformString;
 
 - (BOOL)isSimulator;
 
